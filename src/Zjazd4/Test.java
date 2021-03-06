0ae1d773fb9793c734b4fa8b2cf858fc102c7fb9
aszajko <aszajko25@gmail.com>
2021-03-06 12:24:47 +0100

    Task 3 generyki

--- src/Zjazd4/Test.java
+++ src/Zjazd4/Test.java
@@ -1,8 +1,23 @@
 package Zjazd4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
 
+        List<Object> lista = new ArrayList<>();
+        lista.add("dupa");
+        lista.add(123);
+        lista.add(12.2121);
+
+        System.out.println("lista przyjmująca object : " + lista);
+
+        List<Integer> lista1 = new ArrayList<>();
+        lista1.add(131313);
+        lista1.add(121);
+        lista1.add(Integer.valueOf("dupa"));
+        System.out.println("lista przyjmująca Intiger : " + lista1);
     }
 }
 
